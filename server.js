4a69c6e25a05749c38070e64e118e109f1af4520
chandu32123 <chandu32123@gmail.com>
2016-10-08 18:24:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/ar-1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-1.html.html'));
 });
 
