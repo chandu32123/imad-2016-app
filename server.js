cc2662cf86a61d0a760c65670686313b58bd27b8
chandu32123 <chandu32123@gmail.com>
2016-10-08 18:04:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Article._1.html.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'Article-1.html.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
